592f4bb2e30d79a0ef57ff1592e54fe1c3ea34d7
Harxxki <haruki4dev@gmail.com>
2023-05-21 13:38:42 +0900

    Add icons.

--- src/background.js
+++ src/background.js
@@ -3,11 +3,11 @@ let tabId = null
 chrome.browserAction.onClicked.addListener((tab) => {
   if (tabId === null) {
     tabId = tab.id
-    chrome.browserAction.setIcon({ path: 'on_icon.png' })
+    chrome.browserAction.setIcon({ path: 'icons/on_icon.png' })
     chrome.tabs.sendMessage(tab.id, { command: 'START' })
   } else {
     tabId = null
-    chrome.browserAction.setIcon({ path: 'off_icon.png' })
+    chrome.browserAction.setIcon({ path: 'icons/off_icon.png' })
     chrome.tabs.sendMessage(tab.id, { command: 'STOP' })
   }
 })
@@ -15,6 +15,6 @@ chrome.browserAction.onClicked.addListener((tab) => {
 chrome.tabs.onRemoved.addListener((id) => {
   if (id === tabId) {
     tabId = null
-    chrome.browserAction.setIcon({ path: 'off_icon.png' })
+    chrome.browserAction.setIcon({ path: 'icons/off_icon.png' })
   }
 })
